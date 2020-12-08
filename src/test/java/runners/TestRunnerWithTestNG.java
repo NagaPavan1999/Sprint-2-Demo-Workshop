68becf8e14791f25aca7d467790ceeb41e714cbe
NagaPavan1999 <75241536+NagaPavan1999@users.noreply.github.com>
2020-12-08 17:50:57 +0530

    Update TestRunnerWithTestNG.java

--- src/test/java/runners/TestRunnerWithTestNG.java
+++ src/test/java/runners/TestRunnerWithTestNG.java
@@ -6,7 +6,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(plugin = {"json:target/cucumber.json"},features="Features", glue={"stepDefinitions"},
 //tags={"@Add_to_cart"})
-tags={"@Successful_login"})
+tags={"@Successful_login,@Unsuccessful_login"})
 //tags={"@Successful_Registration,@first_name_field_validation,@last_name_field_validation,@email_field_validation,@password_field_validation,@confirm_password_field_validation"})
 
 public class TestRunnerWithTestNG extends AbstractTestNGCucumberTests{
